cc082edc55f3cb2af98a21eddc124e21a616b6c3
dj08 <juneja.dushyant@gmail.com>
2016-04-25 18:43:06 +0530

    Prime number check algo added for prob 31

---
+++ prob31to41.hs
@@ -0,0 +1,19 @@
+{-
+Problem 31 Determine whether a given integer number is prime.
+
+Example in Haskell:
+P31> isPrime 7
+True
+-}
+
+isPrime :: Integer -> Bool
+
+-- Prime numbers only occur near 6k
+probablePrimes = 2 : 3 : concatMap f (zip [5, 11 ..] [7, 13 ..])
+                 where f (x, y) = [x, y]
+
+isPrime x = foldr g True (takeWhile (<= (ceiling (sqrt (fromInteger x))))
+                          probablePrimes)
+  where
+    g y acc = ( mod x y /= 0 ) && acc
+    
