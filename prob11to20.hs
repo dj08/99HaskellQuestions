6a11da11f9eb0e32c80ec09fa56802595eac2849
dj08 <juneja.dushyant@gmail.com>
2016-04-26 12:02:56 +0530

    Updated a simple, nice solution for prob 18

--- prob11to20.hs
+++ prob11to20.hs
@@ -132,3 +132,20 @@ split :: [a] -> Int -> ([a], [a])
 split xs 0 = ([], xs)
 split [] _ = error "List too short"
 split (x:xs) n = (x:(fst (split xs (n-1))), snd (split xs (n-1)))
+
+{-
+Problem 18 Extract a slice from a list.
+
+Given two indices, i and k, the slice is the list containing the
+elements between the i'th and k'th element of the original list (both
+limits included). Start counting the elements with 1.
+
+Example in Haskell:
+*Main> slice ['a','b','c','d','e','f','g','h','i','k'] 3 7
+"cdefg"
+-}
+
+slice :: (Enum a) => [a] -> Int -> Int -> [a]
+slice [] _ _ = []
+slice xs m n = [(xs !! (m-1)) .. (xs !! (n-1))]
+
