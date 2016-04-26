2270328db5b0ff04ad4c5dc9cf6a14b684c8be93
dj08 <juneja.dushyant@gmail.com>
2016-04-26 12:39:30 +0530

    Added solution to prob 22

--- prob21to28.hs
+++ prob21to28.hs
@@ -13,3 +13,15 @@ insertAt y xs n = [ x | (i, x) <- zip [1..n-1] xs ] ++
 
 -- Bit more efficient... we don't need list comprehension all the time!
 insertAt' y xs n = take (n-1) xs ++ y : drop (n-1) xs
+
+{-
+Problem 22 Create a list containing all integers within a given range.
+
+Example in Haskell:
+Prelude> range 4 9
+[4,5,6,7,8,9]
+-}
+
+range :: Int -> Int -> [Int]
+range m n = [m..n]
+
